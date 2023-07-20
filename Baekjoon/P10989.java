ef742e586f6822ed28d7942d59be0e81e28e1a2e
jjungyeun <wjyddd@naver.com>
2023-07-21 00:39:06 +0900

    [230720] Baekjoon - 10989. 수 정렬하기 3 (풀이 2)
    - 기수 정렬 사용한 풀이
    - 나중에 다시 풀기

--- Baekjoon/P10989.java
+++ Baekjoon/P10989.java
@@ -2,29 +2,36 @@ import java.io.*;
 
 public class P10989 {
     public static void main(String[] args) throws IOException {
-        // 카운팅 정렬
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int[] arr = new int[N];
-        int[] counts = new int[10000+1];
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(br.readLine());
-            counts[arr[i]]++;
         }
 
-        for (int i = 1; i < 10000; i++) {
-            counts[i+1] += counts[i];
-        }
+        int[] tmpArr;
+        for (int i = 0; i < 5; i++) {
+            int pos = (int) Math.pow(10, i);
+            int[] bucket = new int[10];
+            for (int num : arr) {
+                int kthNum = (num / pos) % 10;
+                bucket[kthNum]++;
+            }
 
-        int[] res = new int[N];
-        for (int i = N-1; i >= 0; i--) {
-            int num = arr[i];
-            int idx = (counts[num]--) - 1;
-            res[idx] = num;
+            for (int j = 1; j < 10; j++) {
+                bucket[j] += bucket[j-1];
+            }
+
+            tmpArr = arr.clone();
+            for(int l = N-1; l >= 0; l--) {
+                int kthNum = (tmpArr[l] / pos) % 10;
+                arr[bucket[kthNum]-1] = tmpArr[l];
+                bucket[kthNum]--;
+            }
         }
 
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        for (int num : res) {
+        for (int num : arr) {
             bw.write(num + "\n");
         }
         bw.flush();
