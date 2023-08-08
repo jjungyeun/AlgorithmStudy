abf08dba133c430a787fb0f0435d80b5862aa548
jjungyeun <wjyddd@naver.com>
2023-08-08 19:26:56 +0900

    [230808] Programmers - 야근 지수 (풀이2 추가)

--- Programmers-Java/P230808.java
+++ Programmers-Java/P230808.java
@@ -5,9 +5,9 @@ public class P230808 {
         int[] sales = new int[20000];
         int n = 1000000;
         for (int i = 0; i < 20000; i++) {
-            sales[i] = 50;
+            sales[i] = 500;
         }
-        long ans = solution(n, sales);
+        long ans = solution2(n, sales);
         System.out.println(ans);
     }
 
@@ -42,6 +42,30 @@ public class P230808 {
         long ans = 0;
         for (int i = 0; i < W; i++) {
             ans += (long) sortedWorks[i] * sortedWorks[i];
+        }
+
+        return ans;
+    }
+
+    public static long solution2(int n, int[] works) {
+        int W = works.length;
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        for (int j : works) {
+            pq.add(j);
+        }
+
+        while (n > 0 && !pq.isEmpty()) {
+            int top = pq.poll();
+            if (top == 0) break;
+            pq.add(top -1);
+            n--;
+        }
+
+        long ans = 0;
+        while (!pq.isEmpty()) {
+            int work = pq.poll();
+            ans += (long) work * work;
         }
 
         return ans;
