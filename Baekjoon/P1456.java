a7a0670027d832b28caaced085d36609ab73b006
jjungyeun <wjyddd@naver.com>
2023-08-11 22:19:19 +0900

    [230811] Baekjoon - 1747. 소수&팰린드롬

--- Baekjoon/P1456.java
+++ Baekjoon/P1456.java
@@ -4,30 +4,33 @@ import java.util.Scanner;
 public class P1456 {
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
-        long A = scanner.nextLong();
-        long B = scanner.nextLong();
-
-        int N = (int) Math.sqrt(B);
+        int N = scanner.nextInt();
 
         // false면 소수. true면 소수 아님.
-        boolean[] isNotPrime = new boolean[N+1];
+        int MAX = 10000000;
+        boolean[] isNotPrime = new boolean[MAX+1];
         isNotPrime[1] = true;
-        for (int i = 2; i <= Math.sqrt(N); i++) {
+        for (int i = 2; i <= MAX; i++) {
             if (!isNotPrime[i]) {
-                for (int j = 2; i * j <= N ; j++) {
+                if (i >= N && isPalindrome(i)) {
+                    System.out.println(i);
+                    break;
+                }
+                for (int j = 2; i * j <= MAX ; j++) {
                     isNotPrime[i * j] = true;
                 }
             }
         }
+    }
 
-        int cnt = 0;
-        for (int primeNum = 2; primeNum <= N ; primeNum++) {
-            if (!isNotPrime[primeNum]) {
-                for (int K = 2; Math.pow(primeNum, K) <= B; K++) {
-                    if (Math.pow(primeNum, K) >= A) cnt++;
-                }
+    private static boolean isPalindrome(int num) {
+        String numStr = String.valueOf(num);
+        int len = numStr.length();
+        for (int i = 0; i < len / 2; i++) {
+            if (numStr.charAt(i) != numStr.charAt(len-i-1)){
+                return false;
             }
         }
-        System.out.println(cnt);
+        return true;
     }
 }
